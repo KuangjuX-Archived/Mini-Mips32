1d272fa19932c6fb9edd7c15c26cdd078bff98bb
KuangjuX <18630816527@163.com>
2021-06-03 10:44:20 +0800

    update

--- MiniMIPS32/MiniMIPS32.srcs/sources_1/new/control_unit.sv
+++ MiniMIPS32/MiniMIPS32.srcs/sources_1/new/control_unit.sv
@@ -44,6 +44,20 @@ module main_decoder(
     output logic [1 : 0] aluop
 );
 
+    logic [7 : 0] bundle;
+    assign {reg_write, reg_dst, alu_src, branch, 
+            mem_write, mem_to_reg, aluop} = bundle;
+    
+    always_comb begin 
+        unique case(op)
+            6'b000000: bundle = 8'b1_1_0_0_0_0_10; // R-type
+            6'b100011: bundle = 8'b1_0_1_0_0_1_00; // lw
+            6'b101011: bundle = 8'b0_x_1_0_1_x_00; // sw
+            6'b000100: bundle = 8'b0_x_0_1_0_x_01; // beq
+            default: bundle = 8'bxxxxxxxx; // invalid op
+        endcase
+    end
+
 endmodule: main_decoder
 
 module alu_decoder(
@@ -51,5 +65,21 @@ module alu_decoder(
     input [1 : 0] aluop,
     output logic [2 : 0] alu_control
 );
+
+always_comb begin
+    unique case(aluop)
+        2'b00: alu_control = 3'b010; // add
+        2'b01: alu_control = 3'b110; // sub
+        default: begin
+            unique case(funct)
+                6'b100000: alu_control = 3'b010; // add
+                6'b100010: alu_control = 3'b110; // sub
+                6'b100100: alu_control = 3'b000; // and
+                6'b100101: alu_control = 3'b001; // or
+                6'b101010: alu_control = 3'b111; // slt
+            endcase
+        end
+    endcase
+end
 
 endmodule: alu_decoder
